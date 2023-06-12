353a66a38259acbc722d71f9a89f4993aa560c89
Komal6627 <komalkilledar1996@gmail.com>
2023-06-12 19:06:33 +0530

    Deign Register Page

---
+++ frontend/src/pages/register/Register.jsx
@@ -0,0 +1,28 @@
+import "./register.scss"
+
+const Register = () => {
+  return (
+    <div className="register">
+          <div className="card">
+            <div className="left">
+                <h1>Social App</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae culpa sed numquam officia, commodi animi voluptatum eum sequi labore amet molestiae maiores eius vel pariatur qui accusantium odit quas quos!</p>
+                <span>Do you have account?</span>
+                <button>Register</button>
+            </div>
+            <div className="right">
+                <h1>Register</h1>
+                <form>
+                    <input type="text" placeholder="Username" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <input type="text" placeholder="Name" />
+                    <button>Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Register
