5c1fe3015500e77ca3cf49b21ac41092892ad893
Komal6627 <komalkilledar1996@gmail.com>
2023-06-13 18:28:47 +0530

    Design Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,42 @@
 
 import './App.css';
+import LeftBar from './components/leftBar/LeftBar';
+import Navbar from './components/navbar/Navbar';
+import RightBar from './components/rightBar/RightBar';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-import {BrowserRouter, Route, Routes} from "react-router-dom"; 
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom"; 
 
 function App() {
+  const currentUser = true;
+  const Layout = () =>{
+    return(
+      <div>
+        <Navbar/>
+        <div style={{display: "flex"}}>
+          <LeftBar/>
+          <RightBar/>
+        </div>
+      </div>
+    )
+  }
+
+  const ProtectedRoute = ({children}) =>{
+      if (!currentUser) {
+        return <Navigate to="/login"/>
+      }
+      return children
+  }
+
   return (
     <div >
       <BrowserRouter>
         <Routes>
-        <Route path='/login' Component={Login}/>
-        <Route path='/register' Component={Register}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/' element={<ProtectedRoute>
+          <Layout/>
+        </ProtectedRoute>}/>
         </Routes>
       </BrowserRouter>
         
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./navbar.scss";
+import { Link } from 'react-router-dom'
+import {AiOutlineHome} from "react-icons/ai";
+import {FiMoon} from "react-icons/fi";
+import {BiGridAlt} from "react-icons/bi";
+import {BiSearchAlt2} from "react-icons/bi";
+import {IoPersonOutline} from "react-icons/io5";
+import {HiOutlineMail} from "react-icons/hi";
+import {IoIosNotificationsOutline} from "react-icons/io";
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="left">
+            <Link to="/" style={{textDecoration: "none"}}>
+            <span>GoSocial</span>
+            </Link>
+            <AiOutlineHome/>
+            <FiMoon/>
+            <BiGridAlt/>
+            <div className="search">
+                <BiSearchAlt2/>
+                <input type="text" placeholder='search' />
+            </div>
+        </div>
+        <div className="right">
+            <IoPersonOutline/>
+            <HiOutlineMail/>
+            <IoIosNotificationsOutline/>
+            <div className="user">
+                <img src="https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <span>Komal</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
