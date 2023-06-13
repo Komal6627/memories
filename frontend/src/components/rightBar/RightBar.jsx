da3f0f31003d24030ff888f64924042d17e61cfa
Komal6627 <komalkilledar1996@gmail.com>
2023-06-13 23:55:51 +0530

    Design rightBar component

--- frontend/src/components/rightBar/RightBar.jsx
+++ frontend/src/components/rightBar/RightBar.jsx
@@ -18,6 +18,57 @@ const RightBar = () => {
             </div>
           </div>
         </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+            <img src="https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+            <p>
+                <span>Komal</span> changed the profile
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+            <img src="https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+            <p>
+                <span>Komal</span>
+                change there cover picture
+            </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+            <img src="https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+            <p>
+                <span>Komal</span>
+                change there cover picture
+            </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+            <img src="https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <div className="online"/>
+                <span>Komal</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+            <img src="https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <div className="online"/>
+                <span>Komal</span>
+            </div>
+          </div>
+         
+        </div>
+
       </div>
     </div>
   )
