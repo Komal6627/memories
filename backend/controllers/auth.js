be1f66cb6df3687c276397bf30232c8c0d053da4
Komal6627 <komalkilledar1996@gmail.com>
2023-06-19 23:11:32 +0530

    user register route work successfully

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,31 +1,37 @@
-import {db} from "../connect.js";
+import { db } from '../connect.js'
+import bcrypt from 'bcryptjs'
 
-export const register = (req,res) =>{
-    //Check user if exists
-    const q = "SELECT FROM users WHERE username = ?" 
+export const register = (req, res) => {
+  //Check user if exists
+  const q = 'SELECT * FROM users WHERE username = ?'
 
-    db.query(q,[req.body.username], (err, data) => {
-        if (err) return res.status(500).json(err)
-        if (data.length) return res.status(409).json("User already exists")
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err)
+    if (data.length) return res.status(409).json('User already exists')
 
-        //Hash the password
-    const salt = bcrypt.genSaltSync(10);
+    //Create Users
+    //Hash the password
+    const salt = bcrypt.genSaltSync(10)
     const hashedPassword = bcrypt.hashSync(req.body.password, salt)
 
-    const q = "INSERT INTO users(`username`, `email`, `password`, `name`) VALUE (?)"
+    const q =
+      'INSERT INTO users(`username`, `email`, `password`, `name`) VALUE (?)'
 
-    const values = [req.body.username, req.body.email, hashedPassword, req.body.name]
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashedPassword,
+      req.body.name,
+    ]
 
-    db.query(q,[])
-
-
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err)
+      return res.status(200).json('User has been created')
     })
+  })
 }
-export const login = (req,res) =>{
-
-}
-
-
-export const logout = (req,res) =>{
+export const login = (req, res) => {
     
 }
+
+export const logout = (req, res) => {}
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,10 @@ import { login, logout, register } from "../controllers/auth.js";
 
 const router = express.Router()
 
-router.post("login", login)
+router.post("/login", login)
 
-router.post("register",register)
+router.post("/register",register)
 
-router.post("logout", logout)
+router.post("/logout", logout)
 
 export default router
