84257b3fa80ff7b68331186e1298a96db1824606
Komal6627 <komalkilledar1996@gmail.com>
2023-06-19 20:38:41 +0530

    create connection of sql server and add routes and controller folder

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
-import Express from "express";
-const app = Express()
+import express from "express";
+import userRoutes from "./routes/users.js"
+import authRoutes from "./routes/auth.js"
+const app = express();
+
+//middleware
+app.use(express.json())
+
+app.use("/users", userRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(3000, () => {
     console.log("Server Running");
