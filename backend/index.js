8fb5f154dc36a24b0939f649e3b541f689407584
Komal6627 <komalkilledar1996@gmail.com>
2023-06-20 18:37:24 +0530

    user login and logout routes work successfully

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import userRoutes from "./routes/users.js"
 import authRoutes from "./routes/auth.js"
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
 const app = express();
+
 
 //middleware
 app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
